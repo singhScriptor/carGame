c534cb6834c0c69a7621c5fb8e3407b785f5330b
DHIRAJ KR <dhirajkr1117@gmail.com>
2024-11-22 11:45:22 +0530

    made responsive with joystick of accelerator and left right direction to run the game

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var $ = {
     },
     settings: {
       fps: 60,
-      skySize: 120,
+      skySize: 140,
       ground: {
         size: 350,
         min: 4,
@@ -22,7 +22,7 @@ var $ = {
       },
       road: {
         min: 76,
-        max: 700,
+        max: 750,
       }
     },
     state: {
@@ -32,14 +32,14 @@ var $ = {
       curve: 0,
       currentCurve: 0,
       turn: 1,
-      speed: 27,
+      speed: 30,
       xpos: 0,
       section: 50,
       car: {
         maxSpeed: 50,
         friction: 0.4,
         acc: 0.85,
-        deAcc: 0.5
+        deAcc: 0.9
       },
       keypress: {
         up: false,
